1ead688afd286c625a60d8fc3e93d4e88f8a6497
Fede <Federico@bankingly.com>
2021-10-03 19:17:55 -0300

    Adding function to use localhost without ngrok and modifying gitignore.

--- app/src/main/java/com/example/myapplication/ServiceBuilder.kt
+++ app/src/main/java/com/example/myapplication/ServiceBuilder.kt
@@ -3,18 +3,46 @@ package com.example.myapplication
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://d846-179-24-57-112.ngrok.io/api/")
+        .baseUrl("https://10.0.2.2:5001/api/")
         //.baseUrl("http://192.168.235.249:5000/api/")
         .addConverterFactory(GsonConverterFactory.create())
-        .client(client)
+        .client(getUnsafeOkHttpClient())
         .build()
 
     fun<T> buildService(service: Class<T>): T{
         return retrofit.create(service)
     }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+        })
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }.build()
+    }
+
 }
